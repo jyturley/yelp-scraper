5fda53b1377c8f3a39bb55f7747288d7c5ed03a0
jyt <jyturley@berkeley.edu>
2014-07-30 01:39:02 -0700

    added a way to determine if current soup object is a valid yelp user page on the review tab

--- get_yelp_users.py
+++ get_yelp_users.py
@@ -4,10 +4,20 @@ from urllib import urlopen
 import sys
 import os
 
+# Make sure the page is a yelp user page in the reviews tab.
+def is_valid_user_page(html):
+	if "Yelp" not in html.title.text:
+		return False
 
+	if "All Reviews" not in html.body.h3.text:
+		return False
+
+	return True
 
 def main():
 	assert(len(sys.argv) == 2)
+	user_page_html = BeautifulSoup(urlopen(sys.argv[1]))
+	assert(is_valid_user_page(user_page_html))
 
 if __name__ == "__main__":
 	main()
