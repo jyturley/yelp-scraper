31d39380b7b721810495d6269643bdd6d5af7b5f
jyt <jyturley@berkeley.edu>
2014-07-31 01:10:03 -0700

    added another predicate function to make sure current page is the yelp friends page

--- get_yelp_users.py
+++ get_yelp_users.py
@@ -5,19 +5,21 @@ import sys
 import os
 
 # Make sure the page is a yelp user page in the reviews tab.
-def is_valid_user_page(html):
+def is_valid_yelp_user_review_page(html):
 	if "Yelp" not in html.title.text:
 		return False
-
 	if "All Reviews" not in html.body.h3.text:
 		return False
 
 	return True
 
+def is_valid_yelp_user_friends_page(html):
+	return True if "Friends" in html.body.h3.text else False
+
 def main():
 	assert(len(sys.argv) == 2)
 	user_page_html = BeautifulSoup(urlopen(sys.argv[1]))
-	assert(is_valid_user_page(user_page_html))
+	assert(is_valid_yelp_user_review_page(user_page_html))
 
 if __name__ == "__main__":
 	main()
--- get_yelp_users_test.py
+++ get_yelp_users_test.py
@@ -1,14 +1,19 @@
 import unittest
 from bs4 import BeautifulSoup
 from urllib import urlopen
-from get_yelp_users import is_valid_user_page
-
+from get_yelp_users import is_valid_yelp_user_review_page
+from get_yelp_users import is_valid_yelp_user_friends_page
 
 class GetYelpUsersTest(unittest.TestCase):
-	def test_is_valid_user_page(self):
-		self.assertFalse(is_valid_user_page(get_html('http://www.google.com')))
-		self.assertFalse(is_valid_user_page(get_html('http://www.yelp.com/user_details?userid=1yQUQL1fqOMJzw-cy5mbJg')))
-		self.assertTrue(is_valid_user_page(get_html('http://www.yelp.com/user_details_reviews_self?userid=1yQUQL1fqOMJzw-cy5mbJg')))
+	def test_is_valid_yelp_user_review_page(self):
+		self.assertFalse(is_valid_yelp_user_review_page(get_html('http://www.google.com')))
+		self.assertFalse(is_valid_yelp_user_review_page(get_html('http://www.yelp.com/user_details?userid=1yQUQL1fqOMJzw-cy5mbJg')))
+		self.assertTrue(is_valid_yelp_user_review_page(get_html('http://www.yelp.com/user_details_reviews_self?userid=1yQUQL1fqOMJzw-cy5mbJg')))
+
+	def test_is_valid_yelp_user_friends_page(self):
+		self.assertTrue(is_valid_yelp_user_friends_page(get_html('http://www.yelp.com/user_details_friends?userid=1yQUQL1fqOMJzw-cy5mbJg')))
+		self.assertTrue(is_valid_yelp_user_friends_page(get_html('http://www.yelp.com/user_details_friends?userid=hHEqhNgpBhj9AoHJeW-PnQ')))
+		self.assertFalse(is_valid_yelp_user_friends_page(get_html('http://www.yelp.com/user_details_reviews_self?userid=1yQUQL1fqOMJzw-cy5mbJg')))
 
 def get_html(url):
 	return BeautifulSoup(urlopen(url))
