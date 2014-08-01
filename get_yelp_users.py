510043d7c2c4f1563a3a8099950e0ad17cd9ec01
jyt <jyturley@berkeley.edu>
2014-08-01 00:56:39 -0700

    changed approach on the scrape. Not utilizing urlparse more.

--- get_yelp_users.py
+++ get_yelp_users.py
@@ -1,8 +1,11 @@
 #/usr/bin/python
 from bs4 import BeautifulSoup
 from urllib import urlopen
+from urlparse import urlparse
 import sys
 import os
+
+MAX_USERS = 1000
 
 # Make sure the page is a yelp user page in the reviews tab.
 def is_valid_yelp_user_review_page(html):
@@ -16,10 +19,39 @@ def is_valid_yelp_user_review_page(html):
 def is_valid_yelp_user_friends_page(html):
 	return True if "Friends" in html.body.h3.text else False
 
+def add_user_friends_to_list(user, users_to_visit):
+	pass
+
+def get_yelp_users(seen_users, users_to_visit):
+	while users_to_visit:
+		user = users_to_visit.pop()
+		if user in seen_users:
+			continue
+
+		seen_users.add(user)
+		add_user_friends_to_list(user, users_to_visit)
+		if len(seen_users) > MAX_USERS:
+			break;
+	return seen_users
+
 def main():
 	assert(len(sys.argv) == 2)
-	user_page_html = BeautifulSoup(urlopen(sys.argv[1]))
-	assert(is_valid_yelp_user_review_page(user_page_html))
+	url = urlparse(sys.argv[1])
+	seen_users = set()
+	users_to_visit = []
+
+	assert("www.yelp.com" in url.netloc)
+
+	if "friends" not in url.path:
+		query = url.query
+		url = urlparse("http://www.yelp.com/user_details_friends?%s" % query)
+
+	users_to_visit.append(url.query)
+	get_yelp_users(seen_users, users_to_visit)
+
+	# user_page_html = BeautifulSoup(urlopen(sys.argv[1]))
+	# assert(is_valid_yelp_user_review_page(user_page_html))
+
 
 if __name__ == "__main__":
 	main()
